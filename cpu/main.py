8c8370b3302739db621faf80a51666c461fc8faf
Mariana Bisca <mariana.bisca@gmail.com>
2019-09-06 20:41:46 -0300

    add print state ls

--- cpu/main.py
+++ cpu/main.py
@@ -16,6 +16,18 @@ def print_state(a, x, y, sp, pc, p):
      hex_format(sp, 4),
      bin_format(p)))
 
+def print_state_ls(a, x, y, sp, pc, p, addr, data):
+	print("| pc = %s | a = %s | x = %s | y = %s | sp = %s | p[NV-BDIZC] = %s | MEM[%s] = %s |" % \
+		(hex_format(pc, 4),
+		hex_format(a, 2),
+		hex_format(x, 2),
+		hex_format(y, 2),
+		hex_format(sp, 4),
+		bin_format(p),
+		hex_format(addr, 4),
+		hex_format(data, 2)))
+
+
 def main(argv):
     print_state(2, 2, 2, 2, 2, 2)
 
