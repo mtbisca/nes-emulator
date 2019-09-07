613bc62137d54516bc0aff994eb50a7ec116626a
jpenalber <joao.p.penalber@gmail.com>
2019-09-07 12:11:04 -0300

    Mearge main.py to emu.py

--- cpu/main.py
+++ cpu/main.py
@@ -1,4 +1,47 @@
+import numpy as np
 import sys
+import time
+
+
+###
+### Indicate Registers (use numpy uint16 tosimulate 16 bits of the registers)###
+###
+A = np.uint16(0)
+Y = np.uint16(0)
+X = np.uint16(0)
+PC = np.uint16(0)
+
+
+###
+### Functions of each OP code
+###
+def zero():
+    return "zero"
+ 
+def one():
+    return "one"
+ 
+def two():
+    return "two"
+
+def nothing():
+    return "nothing"
+
+###
+### Dictionary of OP codes
+###
+switcher = {
+     0: zero,
+     1: one,
+     2: two
+}
+ 
+ 
+def run(argument):
+    # Get the function from switcher dictionary
+    func = switcher.get(argument, nothing)
+    # Execute the function of the OP code
+    return func()
 
 def hex_format(value, leading_zeros):
     format_string = "{0:0%sX}" % leading_zeros
@@ -25,11 +68,24 @@ def print_state_ls(a, x, y, sp, pc, p, addr, data):
 		hex_format(sp, 4),
 		bin_format(p),
 		hex_format(addr, 4),
-		hex_format(data, 2)))
+	     hex_format(data, 2)))
 
 
 def main(argv):
-    print_state(2, 2, 2, 2, 2, 2)
+     mem = np.fromfile(sys.argv[1], np.uint8)
+     print_state(2, 2, 2, 2, 2, 2)
+     while (1):
+          # feach instruction
+          instruction = mem[PC]
+          # print op code for debuging
+          print(instruction)
+          # test op code function
+          print(run(instruction))
+          #move PC
+          PC = PC + np.uint16(1)
+
+          # tmporizing
+          time.sleep(1)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
