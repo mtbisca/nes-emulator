9317dbbff095b15539340e42ba69f857e9bc77db
jpenalber <joao.p.penalber@gmail.com>
2019-09-09 19:27:51 -0300

    Fix CUP class and main function

--- cpu/main.py
+++ cpu/main.py
@@ -2,6 +2,8 @@ import numpy as np
 import sys
 import time
 
+running = 1
+
 
 class CPU:
     def __init__(self):
@@ -11,93 +13,100 @@ class CPU:
         self.sp = np.uint16(0)
 
         # State register
-        self.p = None
+        self.p = 0
 
         self.addr = None
         self.data = None
 
         # Data registers
-        self.a = np.uint16(0)
-        self.x = np.uint16(0)
-        self.y = np.uint16(0)
+        self.a = np.uint8(0)
+        self.x = np.uint8(0)
+        self.y = np.uint8(0)
 
-        def _hex_format(value, leading_zeros):
-            format_string = "{0:0%sX}" % leading_zeros
-            return ("0x" + format_string.format(int(value))).lower()
+        self.running = 1
 
-        def _bin_format(value):
-            return "{0:08b}".format(value)
+        ###
+        ### Dictionary of OP codes
+        ###
+        # self.intructions = {
+        #     0: zero,
+        #     1: one,
+        #     2: two
+        #  }
 
-        def print_state():
-            print("| pc = %s | a = %s | x = %s | y = %s | sp = %s | p[NV-BDIZC] = %s |" % \
-                    (_hex_format(self.pc, 4),
-                     _hex_format(self.a, 2),
-                     _hex_format(self.x, 2),
-                     _hex_format(self.y, 2),
-                     _hex_format(self.sp, 4),
-                     _bin_format(self.p)))
+    ###
+    ### Functions of each OP code
+    ###
+    def zero(self):
+        print("zero")
+        self.running = 0
 
-        def print_state_ls():
-        	print("| pc = %s | a = %s | x = %s | y = %s | sp = %s | p[NV-BDIZC] = %s | MEM[%s] = %s |" % \
-            		(_hex_format(self.pc, 4),
-            		 _hex_format(self.a, 2),
-            		 _hex_format(self.x, 2),
-            		 _hex_format(self.y, 2),
-            		 _hex_format(self.sp, 4),
-            		 _bin_format(self.p),
-            		 _hex_format(self.addr, 4),
-            	     _hex_format(self.data, 2)))
+    def one(self):
+        print("o")
+        return "one"
 
-        def execute(opcode):
-            instruction = instruction.get(opcode, does_nothing)
-            instruction()
-            self.pc += np.uint16(1)
+    def two(self):
+        print("t")
+        return "two"
 
 
-###
-### Functions of each OP code
-###
-def zero():
-    return "zero"
+    ###
+    ### Dictionary of OP codes
+    ###
+    instructions = {
+        0: zero,
+        1: one,
+        2: two
+     }
 
-def one():
-    return "one"
+    def _hex_format(self, value, leading_zeros):
+        format_string = "{0:0%sX}" % leading_zeros
+        return ("0x" + format_string.format(int(value))).lower()
 
-def two():
-    return "two"
+    def _bin_format(self, value):
+        return "{0:08b}".format(value)
 
-def nothing():
-    return "nothing"
+    def print_state(self):
+        print("| pc = %s | a = %s | x = %s | y = %s | sp = %s | p[NV-BDIZC] = %s |" % \
+                (self._hex_format(self.pc, 4),
+                 self._hex_format(self.a, 2),
+                 self._hex_format(self.x, 2),
+                 self._hex_format(self.y, 2),
+                 self._hex_format(self.sp, 4),
+                 self._bin_format(self.p)))
 
-###
-### Dictionary of OP codes
-###
-switcher = {
-     0: zero,
-     1: one,
-     2: two
-}
+    def print_state_ls(self):
+    	print("| pc = %s | a = %s | x = %s | y = %s | sp = %s | p[NV-BDIZC] = %s | MEM[%s] = %s |" % \
+        		(self._hex_format(self.pc, 4),
+        		 self._hex_format(self.a, 2),
+        		 self._hex_format(self.x, 2),
+        		 self._hex_format(self.y, 2),
+        		 self._hex_format(self.sp, 4),
+        		 self._bin_format(self.p),
+        		 self._hex_format(self.addr, 4),
+        	     self._hex_format(self.data, 2)))
 
-
-def run(argument):
-    # Get the function from switcher dictionary
-    func = switcher.get(argument, nothing)
-    # Execute the function of the OP code
-    return func()
+    def execute(self, opcode):
+        def nothing(self):
+            print("n")
+            return "nothing"
+        instruction = self.instructions.get(opcode, nothing)
+        instruction(self)
+        self.pc += np.uint8(1)
 
 
 def main(argv):
      mem = np.fromfile(sys.argv[1], np.uint8)
-     print_state(2, 2, 2, 2, 2, 2)
-     while (1):
+     cpu = CPU()
+     # print_state(2, 2, 2, 2, 2, 2)
+     while (cpu.running):
           # feach instruction
-          instruction = mem[PC]
+          instruction = mem[cpu.pc]
           # print op code for debuging
-          print(instruction)
+          cpu.execute(opcode=instruction)
           # test op code function
-          print(run(instruction))
+          cpu.print_state()
           #move PC
-          PC = PC + np.uint16(1)
 
           # tmporizing
           time.sleep(1)
