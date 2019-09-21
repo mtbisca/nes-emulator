12dd014b9722a593195062dd444a30718b20ba6a
Joao Paulo Penalber <joao.p.penalber@gmail.com>
2019-09-21 16:44:03 -0300

    add 3 working tests

--- cpu/tst/brk.s
+++ cpu/tst/brk.s
@@ -12,6 +12,11 @@ MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
 
    .enum $0000
 
+   carLeft                 .dsw 1
+  carRight                .dsw 1
+  carTop                  .dsw 1
+  carBottom               .dsw 1
+
    ;NOTE: declare variables using the DSB and DSW directives, like this:
 
    ;MyVariable0 .dsb 1
@@ -43,8 +48,38 @@ MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
 
    .base $10000-(PRG_COUNT*$4000)
 
+
 Reset:
-   brk ; Abort execution
+ ;Testing compare immediate: Negative = 1, zero = 0
+LDA #128
+
+;Compare with same number: Carry = 1, Zero = 1, Negative = 0
+CMP #128
+
+;Compare with lower number: Carry = 1, Zero = 0, Negative = 0
+CMP #127
+
+;Compare with higher number: Carry = 0, Zero = 0, Negative = 1
+CMP #129
+
+;Testing compare Zero Page: Store 9 in zero page 01
+                                ;10 in zero page 02
+                                ;11 in zero page 03
+LDA #$9
+STA $01
+LDA #$11
+STA $03
+LDA #$10
+STA $02
+
+;Same number
+CMP $02
+;Lower Number
+CMP $01
+;Higher Number
+CMP $03
+
+brk
 
 NMI:
 
