5f32190088abaa9d47a638ab3799ad196f78cbdf
MateusKim <mateuskimura@gmail.com>
2019-09-27 01:31:38 -0300

    compare tests add

---
+++ tst/cmp_immediate.s
@@ -0,0 +1,85 @@
+.org $C000
+
+; Author: tokumaru
+; http://forums.nesdev.com/viewtopic.php?%20p=58138#58138
+;----------------------------------------------------------------
+; constants
+;----------------------------------------------------------------
+PRG_COUNT = 1 ;1 = 16KB, 2 = 32KB
+MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
+
+;----------------------------------------------------------------
+; variables
+;----------------------------------------------------------------
+
+   .enum $0000
+
+   carLeft                 .dsw 1
+  carRight                .dsw 1
+  carTop                  .dsw 1
+  carBottom               .dsw 1
+
+   ;NOTE: declare variables using the DSB and DSW directives, like this:
+
+   ;MyVariable0 .dsb 1
+   ;MyVariable1 .dsb 3
+
+   .ende
+
+   ;NOTE: you can also split the variable declarations into individual pages, like this:
+
+   ;.enum $0100
+   ;.ende
+
+   ;.enum $0200
+   ;.ende
+
+;----------------------------------------------------------------
+; iNES header
+;----------------------------------------------------------------
+
+   .db "NES", $1a ;identification of the iNES header
+   .db PRG_COUNT ;number of 16KB PRG-ROM pages
+   .db $01 ;number of 8KB CHR-ROM pages
+   .db $00|MIRRORING ;mapper 0 and mirroring
+   .dsb 9, $00 ;clear the remaining bytes
+
+;----------------------------------------------------------------
+; program bank(s)
+;----------------------------------------------------------------
+
+   .base $10000-(PRG_COUNT*$4000)
+
+
+Reset:
+ ;Testing compare immediate: Negative = 1, zero = 0
+LDA #128
+
+;Compare with same number: Carry = 1, Zero = 1, Negative = 0
+CMP #128
+
+;Compare with lower number: Carry = 1, Zero = 0, Negative = 0
+CMP #127
+
+;Compare with higher number: Carry = 0, Zero = 0, Negative = 1
+CMP #129
+
+brk
+
+NMI:
+
+   ;NOTE: NMI code goes here
+
+IRQ:
+
+   ;NOTE: IRQ code goes here
+
+;----------------------------------------------------------------
+; interrupt vectors
+;----------------------------------------------------------------
+
+   .org $fffa
+
+   .dw NMI
+   .dw Reset
+   .dw IRQ
---
+++ tst/cmp_indirect_X.s
@@ -0,0 +1,117 @@
+.org $C000
+
+; Author: tokumaru
+; http://forums.nesdev.com/viewtopic.php?%20p=58138#58138
+;----------------------------------------------------------------
+; constants
+;----------------------------------------------------------------
+PRG_COUNT = 1 ;1 = 16KB, 2 = 32KB
+MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
+
+;----------------------------------------------------------------
+; variables
+;----------------------------------------------------------------
+
+   .enum $0000
+
+   carLeft                 .dsw 1
+  carRight                .dsw 1
+  carTop                  .dsw 1
+  carBottom               .dsw 1
+
+   ;NOTE: declare variables using the DSB and DSW directives, like this:
+
+   ;MyVariable0 .dsb 1
+   ;MyVariable1 .dsb 3
+
+   .ende
+
+   ;NOTE: you can also split the variable declarations into individual pages, like this:
+
+   ;.enum $0100
+   ;.ende
+
+   ;.enum $0200
+   ;.ende
+
+;----------------------------------------------------------------
+; iNES header
+;----------------------------------------------------------------
+
+   .db "NES", $1a ;identification of the iNES header
+   .db PRG_COUNT ;number of 16KB PRG-ROM pages
+   .db $01 ;number of 8KB CHR-ROM pages
+   .db $00|MIRRORING ;mapper 0 and mirroring
+   .dsb 9, $00 ;clear the remaining bytes
+
+;----------------------------------------------------------------
+; program bank(s)
+;----------------------------------------------------------------
+
+   .base $10000-(PRG_COUNT*$4000)
+
+
+Reset:
+;posicao 01-02: 0x0041
+;    MEM[0X0041] = 10
+LDA #$41
+STA $02
+
+LDA #$00
+STA $01
+
+LDA #$10
+STA $41
+
+;posicao 03-04: 0x0040
+;    MEM[0X0040] = 09
+LDA #$40
+STA $04
+
+LDA #$00
+STA $03
+
+LDA #$09
+STA $40
+
+;posicao 01-02: 0x0042
+;    MEM[0X0042] = 11
+LDA #$42
+STA $06
+
+LDA #$00
+STA $05
+
+LDA #$11
+STA $42
+
+;A = 10 X = 1
+LDA #$10
+LDX #$01
+
+;Same number
+CMP ($01,X)
+;Lower Number
+CMP ($03,X)
+;Higher Number
+CMP ($05,X)
+
+brk
+
+NMI:
+
+   ;NOTE: NMI code goes here
+
+IRQ:
+
+   ;NOTE: IRQ code goes here
+
+;----------------------------------------------------------------
+; interrupt vectors
+;----------------------------------------------------------------
+
+   .org $fffa
+
+   .dw NMI
+   .dw Reset
+   .dw IRQ
