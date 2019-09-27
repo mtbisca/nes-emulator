0d5af2b357fcf22883fccd6c1ce4b4ebc8209e4e
MateusKim <mateuskimura@gmail.com>
2019-09-27 01:36:22 -0300

    increment tests add

---
+++ tst/inc_absolute.s
@@ -0,0 +1,85 @@
+.org $C000
+
+; Author: tokumaru
+; http://forums.nesdev.com/viewtopic.php?%20p=58138#58138
+;----------------------------------------------------------------
+; constants
+;----------------------------------------------------------------
+PRG_COUNT = 1 ;1 = 16KB, 2 = 32KB
+MIRRORING = %0001 ;%0000 = horizontal, %0001 = vertical, %1000 = four-screen
+
+;----------------------------------------------------------------
+; variables
+;----------------------------------------------------------------
+
+   .enum $0000
+
+   carLeft                 .dsw 1
+  carRight                .dsw 1
+  carTop                  .dsw 1
+  carBottom               .dsw 1
+
+   ;NOTE: declare variables using the DSB and DSW directives, like this:
+
+   ;MyVariable0 .dsb 1
+   ;MyVariable1 .dsb 3
+
+   .ende
+
+   ;NOTE: you can also split the variable declarations into individual pages, like this:
+
+   ;.enum $0100
+   ;.ende
+
+   ;.enum $0200
+   ;.ende
+
+;----------------------------------------------------------------
+; iNES header
+;----------------------------------------------------------------
+
+   .db "NES", $1a ;identification of the iNES header
+   .db PRG_COUNT ;number of 16KB PRG-ROM pages
+   .db $01 ;number of 8KB CHR-ROM pages
+   .db $00|MIRRORING ;mapper 0 and mirroring
+   .dsb 9, $00 ;clear the remaining bytes
+
+;----------------------------------------------------------------
+; program bank(s)
+;----------------------------------------------------------------
+
+   .base $10000-(PRG_COUNT*$4000)
+
+
+Reset:
+LDA #$01
+STA $0001
+LDA #$02
+STA #0002
+LDA #$03
+STA $0003
+
+INC $0001
+INC $0002
+INC $0003
+
+
+brk
+
+NMI:
+
+   ;NOTE: NMI code goes here
+
+IRQ:
+
+   ;NOTE: IRQ code goes here
+
+;----------------------------------------------------------------
+; interrupt vectors
+;----------------------------------------------------------------
+
+   .org $fffa
+
+   .dw NMI
+   .dw Reset
+   .dw IRQ
