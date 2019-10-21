7c3c38ed861354baab003a999b12527db642bfc9
Isabela Telles Furtado Doswaldo <isabela.telles@gitlab.com>
2019-10-21 01:47:29 -0300

    add PPUCTRL

--- ppu/ppu.py
+++ ppu/ppu.py
@@ -17,6 +17,16 @@ class PPU:
         self.sprite_palettes = []
         self.bg_palettes = []
 
+        # Flags controlling PPU operation
+        # TODO: check if there's a default configuration of these flags
+        self.nametable_address = None
+        self.increment_address = None
+        self.sprite_pattern_table_address = None
+        self.background_pattern_table_address = None
+        self.sprite_size = None
+        self.master_slave = None
+        self.nmi_at_vblank = None
+
         # add this address for every write in ppu
         self.address_mirror = 0x400 << mirror
         self.width = 256
@@ -33,6 +43,70 @@ class PPU:
 
         self.update()
 
+    def write_ppuctrl(self, value):
+        """
+        7  bit  0
+        ---- ----
+        VPHB SINN
+        |||| ||||
+        |||| ||++- Base nametable address
+        |||| ||    (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+        |||| |+--- VRAM address increment per CPU read/write of PPUDATA
+        |||| |     (0: add 1, going across; 1: add 32, going down)
+        |||| +---- Sprite pattern table address for 8x8 sprites
+        ||||       (0: $0000; 1: $1000; ignored in 8x16 mode)
+        |||+------ Background pattern table address (0: $0000; 1: $1000)
+        ||+------- Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
+        |+-------- PPU master/slave select
+        |          (0: read backdrop from EXT pins; 1: output color on EXT pins)
+        +--------- Generate an NMI at the start of the
+                   vertical blanking interval (0: off; 1: on)
+        """
+        index = value & 3
+        if index == 0:
+            self.nametable_address = 0x2000
+        elif index == 1:
+            self.nametable_address = 0x2400
+        elif index == 2:
+            self.nametable_address = 0x2800
+        elif index == 3:
+            self.nametable_address = 0x2C00
+
+        remaining_value = value >> 2
+        if remaining_value & 1:
+            self.increment_address = 32
+        else:
+            self.increment_address = 1
+
+        remaining_value >>= 1
+        if remaining_value & 1:
+            self.sprite_pattern_table_address = 0x1000
+        else:
+            self.sprite_pattern_table_address = 0x0000
+
+        remaining_value >>= 1
+        if remaining_value & 1:
+            self.background_pattern_table_address = 0x1000
+        else:
+            self.background_pattern_table_address = 0x0000
+
+        remaining_value >>= 1
+        if remaining_value & 1:
+            self.sprite_size = (8, 16)
+        else:
+            self.sprite_size = (8, 8)
+
+        remaining_value >>= 1
+        if remaining_value & 1:
+            self.master_slave = 1
+        else:
+            self.master_slave = 0
+
+        remaining_value >>= 1
+        if remaining_value & 1:
+            self.nmi_at_vblank = 1
+        else:
+            self.nmi_at_vblank = 0
 
     def load_palettes(self):
         self.bg_palettes = np.array_split(self.VRAM[0X3F00:0x3F10], 4)
